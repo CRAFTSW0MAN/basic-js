55e8880ed40fef4ed26b311a4d5b6273de6ad10a
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2023-12-24 00:02:10 +0600

    feat: add decision for deleteDigit and getCommonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let str1 = "";
+  let str2 = "";
+  if (s1.length > s2.length) {
+    str1 = s1;
+    str2 = s2;
+  } else {
+    str2 = s1;
+    str1 = s2;
+  }
+  let count = 0;
+  const arrStr = str2.split("");
+  for (let i = 0; i <= str1.length; i++) {
+    if (arrStr.includes(str1[i])) {
+      count++;
+      arrStr.splice(arrStr.indexOf(str1[i]), 1);
+      console.log(str1, arrStr);
+    }
+  }
+  return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function deleteDigit(n) {
+  const arr= String(n).split("");
+  const arr2 = [];
+  arr.forEach((number,index)=>{
+    const arr3= Array.from(arr);
+    arr3.splice(index,1);
+    arr2.push(arr3);
+  })
+  const arrSort= arr2.map(item=>Number(item.join('')));
+  return Math.max(...arrSort);
 }
 
 module.exports = {
